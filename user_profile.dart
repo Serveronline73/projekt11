8cf12a75829375c8ac0ee6448e0addd308fff681
dennis <info.dennis.durmus@gmail.com>
2024-09-08 19:42:56 +0200

    Benutzer Profil wurde Angelegt

---
+++ user_profile.dart
@@ -0,0 +1,6 @@
+void benutzerProfilAnzeigen() {
+  print("Benutzerprofil:");
+  print("Name: Max Mustermann");
+  print("Adresse: Musterstraße 1, 12345 Musterstadt");
+  print("Mobilnummer: 015734818527");
+}
