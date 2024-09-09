ea459892e64e8916982d8918d74528fed4a2e1ad
dennis <info.dennis.durmus@gmail.com>
2024-09-09 22:13:14 +0200

    Vorerst fertig wenn ich es nicht weiterhin ändern werde

--- messages.dart
+++ messages.dart
@@ -13,3 +13,14 @@ void nachrichtenSenden() {
   String nachricht = stdin.readLineSync()!;
   print("Nachricht gesendet!");
 }
+
+void nachrichtenUebersetzen() {
+  print("Nachricht übersetzen:");
+  print("Bitte geben Sie die Nachricht ein:");
+  String nachricht = stdin.readLineSync()!;
+  print("Bitte wählen Sie die Zielsprache:");
+  print("1. Englisch");
+  print("2. Französisch");
+  print("3. Spanisch");
+  String option = stdin.readLineSync()!;
+}
